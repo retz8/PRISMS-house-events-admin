d7fa9f9a54b85df86dfecc13e16e13135445f86b
retz8 <ekkicb71@gmail.com>
2023-06-07 23:09:10 -0400

    bug fix

--- src/pages/UsersPage/UpdateUser/UpdateUser.jsx
+++ src/pages/UsersPage/UpdateUser/UpdateUser.jsx
@@ -124,14 +124,13 @@ export default function UpdateUser() {
     const { error, user } = await updateUser(userId, finalUser);
     if (error) {
       // unable to update user
-      if (finalUser.role === "HouseLeader") {
-        if (finalUser.grade !== "12") {
-          window.alert("Grade must be 12");
-          return;
-        } else if (finalUser.grade !== "Faculty") {
-          window.alert("Grade must be Faculty");
-          return;
-        }
+      if (finalUser.role === "HouseLeader" && finalUser.grade !== "Faculty") {
+        window.alert("Grade must be Faculty");
+        return;
+      }
+      if (finalUser.role === "HouseLeader" && finalUser.grade !== "12") {
+        window.alert("Grade must be 12");
+        return;
       }
 
       if (
